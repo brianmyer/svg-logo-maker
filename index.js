f930c3a0063b36e4bd559d617de5324e83be41d7
brianmyer <myer.brian@gmail.com>
2023-08-19 17:37:34 -0400

    shapes,text, and colors all rendering correctly

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const fs = require("fs");
+const SVG = require('./lib/svg')
+const inquirer = require('inquirer');
 
+let example = new SVG('SVG', 'red', 'purple', 'square')
+
+fs.writeFile('shape.svg', example.renderHTML(), function(err){
+    if (err) {
+        console.log(err)
+    }
+} )
