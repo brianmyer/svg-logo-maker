244386460440f6c3f153a771662fc7d2f2de10cd
brianmyer <myer.brian@gmail.com>
2023-08-19 18:20:42 -0400

    commit to push for BCS help

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ const fs = require("fs");
 const SVG = require('./lib/svg')
 const inquirer = require('inquirer');
 
-let example = new SVG('SVG', 'red', 'purple', 'square')
+inquirer
+    .prompt([
+        {
+            type: 'maxlength-input',
+            name: 'text',
+            message: 'Enter your text (max 3 characters)',
+            maxLength: 3
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'What color would you like the font to be?'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'What shape would you like the logo to be?',
+            choices: ['circle', 'square', 'triangle'],
+        },
+        {
+            type: 'input',
+            name: 'fill',
+            message: 'What color would you like the background to be?'
+        },
+    ])
+    .then((data) => {
+        let fileData = new SVG(data.text, data.color, data.fill, data.shape)
+        fs.writeFile('shape.svg', fileData.renderHTML(), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
 
-fs.writeFile('shape.svg', example.renderHTML(), function(err){
-    if (err) {
-        console.log(err)
-    }
-} )
+
+// fs.writeFile('shape.svg', example.renderHTML(), function (err) {
+//     if (err) {
+//         console.log(err)
+//     }
+// })
